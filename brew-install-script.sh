2f5baf7a1eb12965cefb65f68eb9de19e808dcd3
Lucky <lukas.bergman97@gmail.com>
2023-09-27 18:59:46 +0200

    Update brew-install-script.sh

--- brew-install-script.sh
+++ brew-install-script.sh
@@ -3,15 +3,15 @@
 # Homebrew Script for OSX
 # To execute: save and `chmod +x ./brew-install-script.sh` then `./brew-install-script.sh`
 
-echo "Installing brew..."
+echo "-- Installing brew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo "Adding brew to path"
+echo "-- Adding brew to path"
     (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/lucky/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Dev Tools
-echo "Installing development tools..."
+echo "-- Installing development tools..."
 brew install git
 brew install docker
 brew install --cask github
@@ -20,23 +20,23 @@ brew install --cask dotnet-sdk
 brew install --cask visual-studio-code
 
 # Web Tools
-echo "Installing web tools..."
+echo "-- Installing web tools..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 brew install node
 brew install --cask firefox
 brew install --cask google-chrome
 
 # Communication Apps
-echo "Installing communication apps..."
+echo "-- Installing communication apps..."
 brew install --cask slack
 
 # File Storage
-echo "Installing file storage tools..."
+echo "-- Installing file storage tools..."
 brew install --cask google-drive
--- brew install --cask onedrive
+# brew install --cask onedrive
 
 # Other
-echo "Installing everything else..."
+echo "-- Installing everything else..."
 brew install --cask 1password
 brew install --cask alfred
 brew install --cask bartender
@@ -44,4 +44,4 @@ brew install --cask bettertouchtool
 brew install --cask karabiner-elements
 brew install --cask linearmouse
 brew install --cask popclip
--- brew install --cask logi-options-plus
+# brew install --cask logi-options-plus
