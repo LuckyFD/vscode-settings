88ce9fbdb9512d889dcf849ca74637aed85238b1
Lucky <lukas.bergman97@gmail.com>
2023-11-30 10:01:34 +0100

    Update dev-install.ps1

--- dev-install.ps1
+++ dev-install.ps1
@@ -8,16 +8,32 @@ ECHO Configure chocolatey
 choco feature enable -n allowGlobalConfirmation
 choco source add -n=trv -s="https://proget.trafikverket.local/nuget/Chocolatey/"
 
+# Apps
+choco install 1password -y
+choco install notepadplusplus -y
+
 # DEV
 choco install git.portable -y
 choco install nvm.portable -y
-nvm install lts -y
-choco install vscode -y
-choco install visualstudio2022community -y
+choco install nodejs -y
 choco install SqlServerLocalDb -y
 choco install ssms -y
 choco install dotnet-sdk -y
-choco install notepadplusplus
+choco install vscode -y
+choco install visualstudio2022community -y
+
+# Post install
+refreshenv
+#notworking: nvm install lts -y
+git config --global user.name "Lukas Bergman"
+git config --global user.email lukas.bergman@trafikverket.se
+git config --global default.push simple
+git config --global credential.helper wincred
+
+git config --global push.default current
+git config --global http.proxy http://proxyn.trafikverket.local:8080
+npm config set registry "https://proget.trafikverket.local/npm/DefaultNpm/"
+npm config set registry -g "https://proget.trafikverket.local/npm/DefaultNpm/"
 
 
 choco feature disable -n allowGlobalConfirmation
